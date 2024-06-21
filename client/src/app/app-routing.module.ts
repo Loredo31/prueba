8a72f10c8d0c8e153d50b9cd224a30b56af275b4
IsaacCanoH <isac.c.2586@gamil.com>
2024-06-20 21:57:34 -0600

    Actualizado

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,13 +5,19 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RecuperarContrasenaComponent } from './components/recuperar-contrasena/recuperar-contrasena.component';
 import { RegistrarseComponent } from './components/registrarse/registrarse.component';
+import { InicioUsuarioComponent } from './components/inicio-usuario/inicio-usuario.component';
+import { GastoComponent } from './components/gasto/gasto.component';
+import { ServicioComponent } from './components/servicio/servicio.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, 
   { path: 'login', component: LoginComponent }, 
   { path: 'recuperar-contrasena', component: RecuperarContrasenaComponent }, 
   { path: 'registrarse', component: RegistrarseComponent },
-  { path: '**', redirectTo: '/' } 
+  { path: 'inicio-usuario', component: InicioUsuarioComponent },
+  { path: 'gasto', component: GastoComponent},
+  { path: 'servicio', component: ServicioComponent},
+  { path: '**', redirectTo: '/' }
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrarseComponent } from './components/registrarse/registrarse.component';
 import { RecuperarContrasenaComponent } from './components/recuperar-contrasena/recuperar-contrasena.component';
+import { HomeComponent } from './components/home/home.component';
+import { InicioUsuarioComponent } from './components/inicio-usuario/inicio-usuario.component';
+import { GastoComponent } from './components/gasto/gasto.component';
+import { ServicioComponent } from './components/servicio/servicio.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { RecuperarContrasenaComponent } from './components/recuperar-contrasena/
     HomeComponent,
     LoginComponent,
     RegistrarseComponent,
-    RecuperarContrasenaComponent
+    RecuperarContrasenaComponent,
+    InicioUsuarioComponent,
+    GastoComponent,
+    ServicioComponent
   ],
   imports: [
     BrowserModule,
